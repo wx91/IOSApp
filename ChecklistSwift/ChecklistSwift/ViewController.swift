b0962b6b735b35bb6ab2b3cfdda7df34fe84995e
wx91 <766221790@qq.com>
2015-11-23 18:04:40 +0800

    尝试使用swift重写 checklist

---
+++ ChecklistSwift/ChecklistSwift/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ChecklistSwift
+//
+//  Created by 王享 on 15/11/23.
+//  Copyright © 2015年 王享. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
