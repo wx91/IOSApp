9cca93e5f8b4210a12b6cc5e736189cbe92099d9
wx91 <766221790@qq.com>
2015-12-24 17:47:50 +0800

    修改

---
+++ HelloWorld/HelloWorld/TableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TableViewCell.h
+//  HelloWorld
+//
+//  Created by 王享 on 15/11/30.
+//  Copyright © 2015年 wangx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewCell : UITableViewCell
+
+@end
---
+++ HelloWorld/HelloWorld/TableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.m
+//  HelloWorld
+//
+//  Created by 王享 on 15/11/30.
+//  Copyright © 2015年 wangx. All rights reserved.
+//
+
+#import "TableViewCell.h"
+
+@implementation TableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
